a54f2e5d38f1020134b541c5ef1b10435a939d52
AmazonSuperSearch <drdizzle613@gmail.com>
2025-06-22 19:50:15 -0400

    Update share-dropdown.js

--- share-dropdown.js
+++ share-dropdown.js
@@ -14,74 +14,49 @@ document.write(`
 const currentURL = encodeURIComponent(window.location.href);
 const rawURL = window.location.href;
 
+const isAndroid = /Android/i.test(navigator.userAgent);
+
 const shareLinks = () => {
   const platforms = [
     {
       name: "Facebook",
       icon: "facebook",
-      appLink: `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`,
-      webFallback: null // no fallback needed since this is already the reliable URL
+      url: isAndroid
+        ? `intent://www.facebook.com/sharer/sharer.php?u=${currentURL}#Intent;package=com.facebook.katana;scheme=https;end`
+        : `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`
     },
     {
       name: "Twitter / X",
       icon: "twitter",
-      appLink: `twitter://post?message=Check this out ${rawURL}`,
-      webFallback: `https://twitter.com/intent/tweet?url=${currentURL}`
+      url: isAndroid
+        ? `intent://twitter.com/intent/tweet?url=${currentURL}#Intent;package=com.twitter.android;scheme=https;end`
+        : `https://twitter.com/intent/tweet?url=${currentURL}`
     },
     {
       name: "LinkedIn",
       icon: "linkedin",
-      appLink: `linkedin://shareArticle?mini=true&url=${currentURL}`,
-      webFallback: `https://www.linkedin.com/sharing/share-offsite/?url=${currentURL}`
+      url: isAndroid
+        ? `intent://www.linkedin.com/sharing/share-offsite/?url=${currentURL}#Intent;package=com.linkedin.android;scheme=https;end`
+        : `https://www.linkedin.com/sharing/share-offsite/?url=${currentURL}`
     },
     {
       name: "Pinterest",
       icon: "pinterest",
-      appLink: `pinterest://pin/create/button/?url=${currentURL}`,
-      webFallback: `https://www.pinterest.com/pin/create/button/?url=${currentURL}`
+      url: isAndroid
+        ? `intent://pinterest.com/pin/create/button/?url=${currentURL}#Intent;package=com.pinterest;scheme=https;end`
+        : `https://www.pinterest.com/pin/create/button/?url=${currentURL}`
     },
     {
       name: "WhatsApp",
       icon: "whatsapp",
-      appLink: `whatsapp://send?text=Check this out ${rawURL}`,
-      webFallback: `https://wa.me/?text=Check%20this%20out%20${currentURL}`
+      url: isAndroid
+        ? `intent://send?text=Check this out ${rawURL}#Intent;package=com.whatsapp;scheme=whatsapp;end`
+        : `https://wa.me/?text=Check%20this%20out%20${currentURL}`
     },
     {
       name: "Messenger",
       icon: "messenger",
-      appLink: `fb-messenger://share/?link=${currentURL}`,
-      webFallback: `https://www.facebook.com/dialog/send?link=${currentURL}&app_id=966242223397117&redirect_uri=${currentURL}`
+      url: isAndroid
+        ? `intent://www.facebook.com/dialog/send?link=${currentURL}&app_id=966242223397117&redirect_uri=${currentURL}#Intent;package=com.facebook.orca;scheme=https;end`
+        : `https://www.facebook.com/dialog/send?link=${currentURL}&app_id=966242223397117&redirect_uri=${currentURL}`
     },
-    {
-      name: "Telegram",
-      icon: "telegram",
-      appLink: `tg://msg_url?url=${currentURL}`,
-      webFallback: `https://t.me/share/url?url=${currentURL}`
-    },
-    {
-      name: "Email",
-      icon: "envelope",
-      appLink: `mailto:?subject=Check this out&body=Take a look: ${rawURL}`,
-      webFallback: null
-    }
-  ];
-
-  const menu = document.getElementById('shareLinks');
-  if (!menu) return;
-
-  menu.innerHTML = platforms.map(p => {
-    const fallback = p.webFallback
-      ? `setTimeout(() => { try { window.open('${p.webFallback}', '_blank'); } catch(e) {} }, 500);`
-      : '';
-
-    return `
-      <li>
-        <a class="dropdown-item" href="${p.appLink}" onclick="${fallback} return true;" rel="noopener">
-          <i class="bi bi-${p.icon} me-2"></i>${p.name}
-        </a>
-      </li>
-    `;
-  }).join('');
-};
-
-window.addEventListener('DOMContentLoaded', shareLinks);
